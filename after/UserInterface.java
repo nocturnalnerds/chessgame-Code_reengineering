f0b1379151be931ee1d6775d62b67605fda079a3
Williams Sandjaya <133097737+nocturnalnerds@users.noreply.github.com>
2025-06-18 00:20:10 +0700

    SRP Fix in UserInterface

--- after/UserInterface.java
+++ after/UserInterface.java
@@ -1,142 +1,123 @@
+package after;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 
 public class UserInterface extends JPanel implements MouseListener, MouseMotionListener {
-    static int tile = 58, X, Y, newX, newY;
+    static int tile = 58;
+    static int X, Y, newX, newY;
+
+    private final Image chessPieceArray = new ImageIcon("ChessPieces.jpg").getImage();
+
+    public UserInterface() {
+        setPreferredSize(new Dimension(8 * tile, 8 * tile));
+        setBackground(Color.BLACK);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
 
     @Override
-    public void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        this.setBackground(Color.BLACK);
-        this.addMouseListener(this);
-        this.addMouseMotionListener(this);
+        drawBoard(g);
+        drawPieces(g);
+    }
 
+    private void drawBoard(Graphics g) {
         for (int i = 0; i < 64; i += 2) {
-            setVisible(true);
-            setSize(900, 900);
+            int x1 = (i % 8 + (i / 8) % 2) * tile;
+            int y1 = (i / 8) * tile;
+            int x2 = ((i + 1) % 8 - (i / 8) % 2) * tile;
+            int y2 = ((i + 1) / 8) * tile;
+
             g.setColor(new Color(52, 235, 183));
-            g.fillRect((i % 8 + (i / 8) % 2) * tile, (i / 8) * tile, tile, tile);
+            g.fillRect(x1, y1, tile, tile);
+
             g.setColor(new Color(235, 64, 52));
-            g.fillRect(((i + 1) % 8 - (i / 8) % 2) * tile, ((i + 1) / 8) * tile, tile, tile);
+            g.fillRect(x2, y2, tile, tile);
         }
-        Image chessPieceArray = new ImageIcon("ChessPieces.jpg").getImage();
+    }
 
-        for (int i = 0; i < 64; i++) {
-            int j = -1, k = -1;
-            switch (ChessGame.board[i / 8][i % 8]) {
-                case "K":
-                    j = 0;
-                    k = 0;
-                    break;
-                case "k":
-                    j = 0;
-                    k = 1;
-                    break;
-                case "Q":
-                    j = 1;
-                    k = 0;
-                    break;
-                case "q":
-                    j = 1;
-                    k = 1;
-                    break;
-                case "R":
-                    j = 2;
-                    k = 0;
-                    break;
-                case "r":
-                    j = 2;
-                    k = 1;
-                    break;
-                case "B":
-                    j = 3;
-                    k = 0;
-                    break;
-                case "b":
-                    j = 3;
-                    k = 1;
-                    break;
-                case "N":
-                    j = 4;
-                    k = 0;
-                    break;
-                case "n":
-                    j = 4;
-                    k = 1;
-                    break;
-                case "P":
-                    j = 5;
-                    k = 0;
-                    break;
-                case "p":
-                    j = 5;
-                    k = 1;
-                    break;
+    private void drawPieces(Graphics g) {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                String piece = ChessGame.board[row][col];
+                if (!piece.equals(" ")) {
+                    Point cordinate = getCoordinate(piece);
+                    if (cordinate != null) {
+                        g.drawImage(
+                                chessPieceArray,
+                                col * tile, row * tile, (col + 1) * tile, (row + 1) * tile,
+                                cordinate.x * 64, cordinate.y * 64, (cordinate.x + 1) * 64, (cordinate.y + 1) * 64,
+                                this
+                        );
+                    }
+                }
             }
-            if (j != -1 && k != -1) {
-                g.drawImage(chessPieceArray, (i % 8) * tile, (i / 8) * tile, (i % 8 + 1) * tile, (i / 8 + 1) * tile,
-                        j * 64, k * 64, (j + 1) * 64, (k + 1) * 64, this);
-            }
-
         }
+    }
 
+    private Point getCoordinate(String piece) {
+        return switch (piece) {
+            case "K" -> new Point(0, 0);
+            case "k" -> new Point(0, 1);
+            case "Q" -> new Point(1, 0);
+            case "q" -> new Point(1, 1);
+            case "R" -> new Point(2, 0);
+            case "r" -> new Point(2, 1);
+            case "B" -> new Point(3, 0);
+            case "b" -> new Point(3, 1);
+            case "N" -> new Point(4, 0);
+            case "n" -> new Point(4, 1);
+            case "P" -> new Point(5, 0);
+            case "p" -> new Point(5, 1);
+            default -> null;
+        };
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        if (e.getX() < 8 * tile && e.getY() < 8 * tile) {
-
+        if (inBounds(e.getX(), e.getY())) {
             X = e.getX();
             Y = e.getY();
-            repaint();
         }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        if (e.getX() < 8 * tile && e.getY() < 8 * tile) {
+        if (inBounds(e.getX(), e.getY()) && e.getButton() == MouseEvent.BUTTON1) {
             newX = e.getX();
             newY = e.getY();
-            String dragpiece;
-            if (e.getButton() == MouseEvent.BUTTON1) {
-                if (newY / tile == 0 && Y / tile == 1 && "P".equals(ChessGame.board[Y / tile][X / tile])) {
-                    dragpiece = "" + X / tile + newX / tile + ChessGame.board[newY / tile][newX / tile] + "Q" + "P";
-                } else if (newY / tile == 7 && Y / tile == 6 && "p".equals(ChessGame.board[Y / tile][X / tile])) {
-                    dragpiece = "" + X / tile + newX / tile + ChessGame.board[newY / tile][newX / tile] + "q" + "p";
-                } else {
-                    dragpiece = "" + Y / tile + X / tile + newY / tile + newX / tile
-                            + ChessGame.board[newY / tile][newX / tile];
-                }
-
-                if (ChessGame.possMoves().replaceAll(dragpiece, " ").length() < ChessGame.possMoves().length()) {
-                    ChessGame.movePiece(dragpiece);
-                }
-
+            String move = createMoveCommand();
+            if (ChessGame.possMoves().contains(move)) {
+                ChessGame.movePiece(move);
             }
             repaint();
         }
     }
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
+    private String createMoveCommand() {
+        String piece = ChessGame.board[Y / tile][X / tile];
+        String target = ChessGame.board[newY / tile][newX / tile];
+
+        if (piece.equals("P") && newY / tile == 0 && Y / tile == 1) {
+            return "" + X / tile + newX / tile + target + "Q" + piece;
+        }
+        if (piece.equals("p") && newY / tile == 7 && Y / tile == 6) {
+            return "" + X / tile + newX / tile + target + "q" + piece;
+        }
+
+        return "" + Y / tile + X / tile + newY / tile + newX / tile + target;
     }
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+    private boolean inBounds(int x, int y) {
+        return x < 8 * tile && y < 8 * tile;
     }
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-    }
+    @Override public void mouseEntered(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent e) {}
+    @Override public void mouseDragged(MouseEvent e) {}
+    @Override public void mouseMoved(MouseEvent e) {}
+    @Override public void mouseClicked(MouseEvent e) {}
 }
